8b16c399f2f32e4287d8a1588cd03fc34ee38169
Nora Pence <nora.pence@bellevuecollege.edu>
2025-02-08 22:15:10 -0800

    Update example.js

--- m05/example.js
+++ m05/example.js
@@ -1,14 +1,19 @@
-var table = 3;             // Unit of table
+//Ask user for number 1 - 10
+var table = prompt("Enter a number form 0 to 10");            
 
-operator = GetValueFromUser("operator");
-table = GetValueFromUser("table")
+//operator = GetValueFromUser("operator");
+//table = GetValueFromUser("table")
+table = parseInt(table) // Make into integer
 
+if (isNaN(table) || table === null) {
+    table = 0;
+} // turns null of Nan Response to 0
 
-// Write the message into the page
-var el = document.getElementById('blackboard');
-el.innerHTML = GetTableContent(operator, table);
+//Write the message into the page
+var el = document.getElementById('blackboard'); 
+el.innerHTML = GetTableContent(table);
 
-function GetValueFromUser(valueType) {
+/*function GetValueFromUser(valueType) {
   var greetingMessage = 'Hello. How are you?';
   if (valueType == "operator") {
     greetingMessage += "Enter addition or multiplication";
@@ -18,18 +23,18 @@ function GetValueFromUser(valueType) {
     greetingMessage += "Enter table";
   }
   return prompt(greetingMessage);
-}
+}*/
 
-function GetTableContent(operator, table) {
+function GetTableContent(table) {
   var i = 1;                 // Set counter to 1
-  var msg = '';              // Message  
-  if (operator === 'addition') {
+  var msg = '<h2>Multiplication Table</h2>';              // Heading message
+ /* if (operator === 'addition') {
     // Do addition
     while (i < 11) {
       msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
       i++;
     }
-  } else {
+  } else {*/
     // Do multiplication
     while (i < 11) {
       msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
