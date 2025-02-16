9fa70e3dd748c7d36415f1fba934a00bb5c7dca1
Nora Pence <nora.pence@bellevuecollege.edu>
2025-02-15 23:06:50 -0800

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -1,48 +1,92 @@
-function createRhombus(pHeight, pSymbol, pColorEven, pColorOdd) {
-    var rLine = "";
-    var white = "#ffffff"; // Define the color of the white spaces
+  function createRhombus(pHeight, pSymbol, pColorEven, pColorOdd) {
+            var rLine = "";
+            var white = "#ffffff";
 
-    // Top Left 
-    for (let i = 1; i <= pHeight; i++) {
-        rLine += "<p>";
-        
-        // Create each line with white spaces
-        for (let j = 0; j < pHeight - i; j++) {
-            rLine += "<span style='color:" + white + ";'>" + " " + "</span>";
-        }
-        
-        // Add the colored symbols with alternating colors
-        for (let j = 0; j < i; j++) {
-            if (j % 2 === 0) { // Even position
-                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            } else { // Odd position
-                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+            // Top Left 
+            for (let i = 1; i <= pHeight; i++) {
+                rLine += "<p>";
+                
+                // White spaces for left alignment
+                for (let j = 0; j < pHeight - i; j++) {
+                    rLine += "<span style='color:" + white + ";'>" + " " + "</span>";
+                }
+                
+                // Colored symbols
+                for (let j = 0; j < i; j++) {
+                    if (j % 2 === 0) {
+                        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+                    } else {
+                        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+                    }
+                }
+
+                rLine += "</p>";
             }
-        }
-        
-        rLine += "</p>";
-    }
 
-    // Bottom Left 
-    for (let i = pHeight - 1; i > 0; i--) {
-        rLine += "<p>";
-        
-        // Create each line with white spaces
-        for (let j = 0; j < pHeight - i; j++) {
-            rLine += "<span style='color:" + white + ";'>" + " " + "</span>";
-        }
-        
-        // Add the colored symbols with alternating colors
-        for (let j = 0; j < i; j++) {
-            if (j % 2 === 0) { // Even position
-                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            } else { // Odd position
-                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+            // Top Right 
+            for (let i = 1; i <= pHeight; i++) {
+                rLine += "<p>";
+                
+                // Symbols with alternating colors
+                for (let j = 0; j < pHeight - i; j++) {
+                    rLine += "<span style='color:" + white + ";'>" + " " + "</span>";
+                }
+
+                for (let j = 0; j < i; j++) {
+                    if (j % 2 === 0) {
+                        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+                    } else {
+                        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+                    }
+                }
+
+                rLine += "</p>";
             }
-        }
-        
-        rLine += "</p>";
-    }
 
-    return rLine; // Return the final output
-}
+            // Bottom Left 
+            for (let i = pHeight - 1; i > 0; i--) {
+                rLine += "<p>";
+                
+                // White spaces 
+                for (let j = 0; j < pHeight - i; j++) {
+                    rLine += "<span style='color:" + white + ";'>" + " " + "</span>";
+                }
+                
+                // Colored symbols 
+                for (let j = 0; j < i; j++) {
+                    if (j % 2 === 0) {
+                        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+                    } else {
+                        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+                    }
+                }
+
+                rLine += "</p>";
+            }
+
+            // Bottom Right 
+            for (let i = pHeight - 1; i > 0; i--) {
+                rLine += "<p>";
+                
+               
+                for (let j = 0; j < pHeight - i; j++) {
+                    rLine += "<span style='color:" + white + ";'>" + " " + "</span>";
+                }
+
+                for (let j = 0; j < i; j++) {
+                    if (j % 2 === 0) {
+                        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+                    } else {
+                        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+                    }
+                }
+
+                rLine += "</p>";
+            }
+
+            return rLine; 
+        }
+
+        // Generate rhombus 
+        document.getElementById("rhombus").innerHTML = createRhombus(pHeight, pSymbol, pColorEven, pColorOdd);
+    
