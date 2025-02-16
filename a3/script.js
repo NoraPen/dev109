c8888fc488112c1443b79b4b6e4fb88ad491b028
Nora Pence <nora.pence@bellevuecollege.edu>
2025-02-15 22:42:57 -0800

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -6,7 +6,8 @@
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 upRight(pHeight, pColorEven, pColorOdd, pSymbol);
 downRight(pHeight, pColorEven, pColorOdd, pSymbol);
-
+upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -52,7 +53,57 @@ rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
 rLine +="</p>";
 // console.log(rLine);
 
+  
+
 }
 
 document.getElementById("downRight").innerHTML = rLine;
+}
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (i = 0; i < pHeight; i++) {
+    rLine += "<p>";
+    // Add spaces for left alignment
+    for (s = 0; s < pHeight - i - 1; s++) {
+      rLine += "&nbsp;"; // space character
+    }
+    // Create each line on the Rhombus
+    for (j = 0; j <= i; j++) {
+      // Is the position even or odd so we change the color
+      if (j % 2)
+        // even
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      else
+        // odd
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+    }
+    rLine += "</p>";
+  }
+
+  document.getElementById("upLeft").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (i = pHeight; i > 0; i--) {
+    rLine += "<p>";
+    // Add spaces for left alignment
+    for (s = 0; s < pHeight - i; s++) {
+      rLine += "&nbsp;"; // space character
+    }
+    // Create each line on the Rhombus
+    for (j = 0; j < i; j++) {
+      // Is the position even or odd so we change the color
+      if (j % 2)
+        // even
+        rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      else
+        // odd
+        rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+    }
+    rLine += "</p>";
+  }
+
+  document.getElementById("downLeft").innerHTML = rLine;
 }
