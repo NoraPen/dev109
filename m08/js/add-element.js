26e24987be8814289ee0ad2bf2094c308bb2af04
Nora Pence <nora.pence@bellevuecollege.edu>
2025-03-02 00:14:51 -0800

    Update add-element.js

--- m08/js/add-element.js
+++ m08/js/add-element.js
@@ -1,36 +1,37 @@
- function addNewItem() {
-            var input = document.getElementById("itemInput");
-            var itemText = input.value.trim();
-            
-           
-             var list = document.getElementById("todo");
-             var newItem = document.createElement("li");
-             newItem.textContent = itemText;
-             list.appendChild(newItem);
-                
-             input.value = "";
-            
-        }
+function addNewItem() {
+     var input = document.getElementById("itemInput");
+     var itemText = input.value.trim();
 
 
-// Create a new element and store it in a variable.
-  var newEl = document.createElement('li');
+     var list = document.getElementById("todo");
+     var newItem = document.createElement("li");
+     newItem.textContent = itemText;
+     list.appendChild(newItem);
 
-// Create a text node and store it in a variable.
-  var newText = document.createTextNode('quinoa');
+     input.value = "";
 
-// Attach the new text node to the new element.
-  newEl.appendChild(newText);
+ }
 
-// Find the position where the new element should be added.
-  var position = document.getElementsByTagName('ul')[0];
 
-// Insert the new element into its position.
-  position.appendChild(newEl);
+ // Create a new element and store it in a variable.
+ var newEl = document.createElement('li');
 
-var input = document.getElementById("itemInput");
-input.addEventListener("keypress", function(event) {
-       if (event.key === "Enter") {
-             addNewItem(); 
-              }
-}
+ // Create a text node and store it in a variable.
+ var newText = document.createTextNode('quinoa');
+
+ // Attach the new text node to the new element.
+ newEl.appendChild(newText);
+
+ // Find the position where the new element should be added.
+ var position = document.getElementsByTagName('ul')[0];
+
+ // Insert the new element into its position.
+ position.appendChild(newEl);
+
+ var input = document.getElementById("itemInput");
+ input.addEventListener("keypress", function(event) {
+     if (event.key === "Enter") {
+         addNewItem(); 
+         event.preventDefault(); 
+     }
+ });
