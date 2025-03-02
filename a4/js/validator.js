0a2bcfd6d864028a632c2621f492f61354bba961
Nora Pence <nora.pence@bellevuecollege.edu>
2025-03-01 19:02:06 -0800

    Update validator.js

--- a4/js/validator.js
+++ a4/js/validator.js
@@ -1,6 +1,15 @@
 function isValid() {
     if (firstName() &&
-       lastName()
+        lastName() &&
+        emailValidation() &&
+        phoneValidation() &&
+        usernameValidation() &&
+        passwordValidaiton() &&
+        addressValidation() &&
+        cityValidation() &&
+        stateValiadtion() &&
+        countryValidation() &&
+        zipcodeValidation()
     )
     return true;
     else
@@ -68,5 +77,151 @@ function lastName(){
     //5) return status of each field
     return (validLastname);
 };
+
+
+//EMAIL
+Email.addEventListener('blur', emailValidation, false);
+function emailValidation(){
+      var validEmail = false;
+      var userEmail = document.getElementById("Email").value;
+      var errorMessages = "";
+      var atpos = userEmail.indexOf("@");
+      var dotpos = userEmail.lastIndexOf(".");
+      if (userEmail === "" || atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length) {
+        errorMessages += "<p>Invalid email</p>";
+      } else {
+        validEmail = true;
+      }
+      document.getElementById("emailError").innerHTML = errorMessages;
+      return validEmail;
+    }
+
+//PHONE
+Phone.addEventListener('blur', phoneValidation, false);
+function phoneValidation(){
+    var validPhone = false;
+    var errorMessages = "";
+    var phone = document.getElementById("phone").value;
+    
+    var numbers = /^[0-9]+$/;
+    
+    if (phone === "" || phone.length > 15 || !phone.match(numbers)) {
+        errorMessages += "<p>Invalid phone number. Must be numerical and maximum 15 digits.</p>";
+    } else {
+        validPhone = true;
+    }
+    
+    document.getElementById("phoneError").innerHTML = errorMessages;
+    return validPhone;
+}
+
+
+//USERNAME
+Username.addEventListener('blur', usernameValidation, false);
+function usernameValidation(){
+      var validUsername = false;
+      var username = document.getElementById("Username").value;
+      var errorMessages = "";
+      if (username === "" || username.length > 12) {
+        errorMessages += "<p>Username is required and cannot exceed 12 characters</p>";
+      } else {
+        validUsername = true;
+      }
+      document.getElementById("usernameError").innerHTML = errorMessages;
+      return validUsername;
+    }
+
+//PASSWORD
+Password.addEventListener('blur', passwordValidation, false);
+function passwordValidation(){
+      var validPassword = false;
+      var password = document.getElementById("Password").value;
+      var errorMessages = "";
+      if (password === "" || password.length > 7) {
+        errorMessages += "<p>Password is required and cannot exceed 7 characters</p>";
+      } else {
+        validPassword = true;
+      }
+      document.getElementById("passwordError").innerHTML = errorMessages;
+      return validPassword;
+    }
+
+// ADDRESS
+Address.addEventListener('blur', addressValidation, false);
+function addressValidation(){
+      var validAddress = false;
+      var address = document.getElementById("Address").value;
+      var errorMessages = "";
+      if (address === "") {
+        errorMessages += "<p>Address is required</p>";
+      } else {
+        validAddress = true;
+      }
+      document.getElementById("addressError").innerHTML = errorMessages;
+      return validAddress;
+    }
+
+
+//CITY
+City.addEventListener('blur', cityValidation, false);
+function cityValidation(){
+      var validCity = false;
+      var city = document.getElementById("City").value;
+      var errorMessages = "";
+      if (city === "") {
+        errorMessages += "<p>City is required</p>";
+      } else {
+        validCity = true;
+      }
+      document.getElementById("cityError").innerHTML = errorMessages;
+      return validCity;
+    }
+
+//STATE
+State.addEventListener('change', stateValidation, false);
+function stateValidation(){
+      var validState = false;
+      var state = document.getElementById("State").value;
+      var errorMessages = "";
+      if (state === "") {
+        errorMessages += "<p>Please select a state</p>";
+      } else {
+        validState = true;
+      }
+      document.getElementById("stateError").innerHTML = errorMessages;
+      return validState;
+    }
+
+//COUNTRY
+Country.addEventListener('change', countryValidation, false);
+function countryValidation(){
+      var validCountry = false;
+      var country = document.getElementById("Country").value;
+      var errorMessages = "";
+      if (country === "") {
+        errorMessages += "<p>Please select a country</p>";
+      } else {
+        validCountry = true;
+      }
+      document.getElementById("countryError").innerHTML = errorMessages;
+      return validCountry;
+    }
+
+//ZIPCODE
+ZipCode.addEventListener('blur', zipcodeValidation, false);
+function zipcodeValidation(){
+      var validZipcode = true; 
+      var zipcode = document.getElementById("ZipCode").value;
+      var errorMessages = "";
+      var country = document.getElementById("Country").value;
+      if (country === "USA") {
+        if (zipcode === "" || zipcode.length > 5 || !/^[0-9]+$/.test(zipcode)) {
+          errorMessages += "<p>Invalid Zip Code. Must be numerical and up to 5 digits.</p>";
+          validZipcode = false;
+        }
+      }
+      document.getElementById("zipcodeError").innerHTML = errorMessages;
+      return validZipcode;
+    }
 
 
