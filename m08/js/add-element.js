6b298673c16e1de9637eb9810915a287ff7dec79
Nora Pence <nora.pence@bellevuecollege.edu>
2025-03-01 23:14:48 -0800

    Update add-element.js

--- m08/js/add-element.js
+++ m08/js/add-element.js
@@ -1,14 +1,30 @@
+ function addNewItem() {
+            var input = document.getElementById("itemInput");
+            var itemText = input.value.trim();
+            
+            if (itemText !== "") {
+                var list = document.getElementById("todo");
+                var newItem = document.createElement("li");
+                newItem.textContent = itemText;
+                list.appendChild(newItem);
+                
+                input.value = "";
+            }
+        }
+
+
 // Create a new element and store it in a variable.
-var newEl = document.createElement('li');
+  var newEl = document.createElement('li');
 
 // Create a text node and store it in a variable.
-var newText = document.createTextNode('quinoa');
+  var newText = document.createTextNode('quinoa');
 
 // Attach the new text node to the new element.
-newEl.appendChild(newText);
+  newEl.appendChild(newText);
 
 // Find the position where the new element should be added.
-var position = document.getElementsByTagName('ul')[0];
+  var position = document.getElementsByTagName('ul')[0];
 
 // Insert the new element into its position.
-position.appendChild(newEl);
+  position.appendChild(newEl);
+
